ccbf860fed52385d3ff6c0c73b2d2fb7043016a6
Elecwizer <elecwizer@gmail.com>
2026-02-14 23:30:56 +0300

    bullets and player movement fully functional

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,17 +1,101 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    
-    public float Speed;
+    float horizontalInput;
+    float verticalInput;
+
+    // Movement variables
+    [SerializeField] float _speed;
+    [SerializeField] float _turnSpeed;
+
+    // Shooting variables
+    [SerializeField] GameObject _bullet;
+    Vector3 _bulletOffset = new Vector3(-0.002f, 3.306f, 4.834f);
+    int _poolSize = 20;
+    [SerializeField] float _bulletSpeed;
+    readonly Queue<GameObject> _bulletPool = new Queue<GameObject>();
+
     void Start()
     {
-        Speed = 10.0f;
+        _speed = 20.0f;
+        _turnSpeed = 25.0f;
+        _bulletSpeed = 25.0f;
+
+        // Filling pool
+        for (int i = 0; i < _poolSize; i++)
+        {
+            GameObject b = Instantiate(_bullet);
+            b.transform.position = new Vector3(0f, -5f, 0f);
+            b.SetActive(false);
+
+            // Ensure bullet can return itself to THIS pool
+            var pooled = b.GetComponent<PooledBullet>();
+            if (pooled == null) pooled = b.AddComponent<PooledBullet>();
+            pooled.Init(this);
+
+            _bulletPool.Enqueue(b);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime * Speed);
+        verticalInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        // Move or rotate the tank depending on input
+        transform.Translate(Vector3.forward * Time.deltaTime * _speed * verticalInput);
+        transform.Rotate(Vector3.up, Time.deltaTime * _turnSpeed * horizontalInput);
+
+        // Fire when spacebar is pressed
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            FireBullet();
+        }
     }
+
+        void FireBullet()
+    {
+        GameObject b = GetBulletFromPool();
+        if (b == null) return;
+
+        // Spawn at player position going forward
+        b.transform.position = transform.TransformPoint(_bulletOffset); // small offset so it doesnt collide with player
+        b.transform.rotation = transform.rotation;
+
+        // Give it velocity in forward direction
+        Rigidbody rb = b.GetComponent<Rigidbody>();
+        if (rb != null)
+        {
+            rb.linearVelocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
+            rb.linearVelocity = b.transform.forward * _bulletSpeed;
+        }
+
+        b.SetActive(true);
+    }
+
+    GameObject GetBulletFromPool()
+    {
+        if (_bulletPool.Count == 0) return null;
+        var b = _bulletPool.Dequeue();
+        return b;
+    }
+
+    public void ReturnBulletToPool(GameObject bullet)
+    {
+        // Reset physics just in case
+        Rigidbody rb = bullet.GetComponent<Rigidbody>();
+        if (rb != null)
+        {
+            rb.linearVelocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
+        }
+
+        bullet.SetActive(false);
+        _bulletPool.Enqueue(bullet);
+    }
+
 }
---
+++ Assets/Scripts/PooledBullet.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class PooledBullet : MonoBehaviour
+{
+    private PlayerController _poolOwner;
+
+    public void Init(PlayerController owner)
+    {
+        _poolOwner = owner;
+    }
+
+    void Update()
+    {
+        // Return to pool if out of bounds
+        if (Mathf.Abs(transform.position.x) > 20 ||
+            Mathf.Abs(transform.position.y) > 10 ||
+            Mathf.Abs(transform.position.z) > 200)
+        {
+            ReturnToPool();
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        // Return to pool when hitting something
+        ReturnToPool();
+    }
+
+    void ReturnToPool()
+    {
+        if (_poolOwner != null)
+            _poolOwner.ReturnBulletToPool(gameObject);
+        else
+            gameObject.SetActive(false); // fallback
+    }
+}
